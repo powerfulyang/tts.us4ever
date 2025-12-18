c4cb5ae9ed6f78a8038aaddbbb1e9c412647adc1
eleven <hutyxxx@gmail.com>
2025-12-18 22:08:18 +0800

    docker

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,17 @@ FROM pytorch/pytorch:2.9.1-cuda12.8-cudnn9-runtime
 WORKDIR /app
 
 # 安装依赖
-# COPY requirements.txt .
-# RUN pip install --no-cache-dir -r requirements.txt
+COPY requirements.txt .
+RUN --mount=type=cache,target=/root/.cache/pip \
+ pip install -r requirements.txt
 
 # 复制应用程序代码
-# COPY api.py .
-# COPY embedding.py .
+COPY api.py .
+COPY embedding.py .
 
 # 暴露端口
-# EXPOSE 8001
+EXPOSE 8001
 
 # 启动应用
-# CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "8001"]
-
-# 判断是否有 torch cuda 环境
-CMD [ "python", "-c", "import torch; print(torch.cuda.is_available())" ]
+CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "8001"]
 
--- embedding.py
+++ embedding.py
@@ -1,19 +1,25 @@
 import torch
 from sentence_transformers import SentenceTransformer
 
-# 加载BGE-M3模型
-model = SentenceTransformer('BAAI/bge-m3')
+model = None
+default_embedding = None
 
-# 获取模型的第一个参数的设备
-device = next(model.parameters()).device
-if device.type == 'cuda':
-    print(f"模型在 GPU 上运行，设备：{device}")
-    # 进一步显示 GPU 信息
-    print(f"GPU 名称：{torch.cuda.get_device_name(device.index if device.index is not None else 0)}")
-else:
-    print("模型在 CPU 上运行")
 
-default_embedding = model.encode("unknown", normalize_embeddings=True).tolist()
+def load_model():
+    global model, default_embedding
+    # 加载BGE-M3模型
+    model = SentenceTransformer('BAAI/bge-m3')
+
+    # 获取模型的第一个参数的设备
+    device = next(model.parameters()).device
+    if device.type == 'cuda':
+        print(f"模型在 GPU 上运行，设备：{device}")
+        # 进一步显示 GPU 信息
+        print(f"GPU 名称：{torch.cuda.get_device_name(device.index if device.index is not None else 0)}")
+    else:
+        print("模型在 CPU 上运行")
+
+    default_embedding = model.encode("unknown", normalize_embeddings=True).tolist()
 
 
 def embedding_text(text: str):
@@ -22,6 +28,9 @@ def embedding_text(text: str):
     :param text: 输入文本
     :return: 嵌入表示
     """
+    if model is None:
+        load_model()
+
     # 检查输入是否为空
     if not text.strip():
         # 或者返回预计算的默认向量
